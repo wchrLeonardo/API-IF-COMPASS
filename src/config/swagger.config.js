8291b5c2bfda22197525593ed9445264b1d3b769
wicher_leonardo <leonardo.wlferreira@gmail.com>
2025-09-15 11:37:54 -0300

    API simulando uma IF(instituição financeira - COMPASS CHALLENGE)

--- src/config/swagger.config.js
+++ src/config/swagger.config.js
@@ -1,14 +1,25 @@
-import fs from 'fs';
-import yaml from 'js-yaml';
-import path from 'path';
+import swaggerJSDoc from 'swagger-jsdoc';
 import { fileURLToPath } from 'url';
+import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Carregar o arquivo YAML
-const yamlPath = path.join(__dirname, '../../docs/openapi.yaml');
-const yamlContent = fs.readFileSync(yamlPath, 'utf8');
-const specs = yaml.load(yamlContent);
+const options = {
+    definition: {
+        openapi: '3.1.0',
+        info: {
+            title: 'API IF COMPASS',
+            version: '1.0.0'
+        },
+    },
+    apis: [
+        path.join(__dirname, '../../docs/openapi.yaml'),
+        path.join(__dirname, '../../docs/paths/*.yaml'),
+        path.join(__dirname, '../../docs/schemas/*.yaml')
+    ]
+};
 
-export default specs;
+const swaggerSpec = swaggerJSDoc(options);
+
+export default swaggerSpec;
--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -3,10 +3,10 @@ import transactionController from '../controllers/transaction.controller.js';
 
 const transactionRoutes = Router();
 
-transactionRoutes.post('transactions/accounts/:id_account', transactionController.create);
+transactionRoutes.post('/transactions/accounts/:id_account', transactionController.create);
 
-transactionRoutes.get('transactions/accounts/:id_account', transactionController.getAllByAccountId);
+transactionRoutes.get('/transactions/accounts/:id_account', transactionController.getAllByAccountId);
 
-transactionRoutes.get('transactions/:id', transactionController.getById);
+transactionRoutes.get('/transactions/:id', transactionController.getById);
 
 export default transactionRoutes;
